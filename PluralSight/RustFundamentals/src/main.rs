ac6b2cad30c8802a8f766a756b4ba513d085d958
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-11-24 15:57:06 -0600

    PluralSight/RustFundamentals: refactor location / waypoint

--- PluralSight/RustFundamentals/src/main.rs
+++ PluralSight/RustFundamentals/src/main.rs
@@ -1,36 +1,56 @@
 const EARTH_RADIUS: f64 = 6371.0;
 
-struct Waypoint {
+struct Location {
 	latitude: f64,
 	longitud: f64,
 }
 
-/// Using the Haversine Formula from the course
-fn distance(a: &Waypoint, b: &Waypoint) -> f64 {
-	let a_latitude_rad = a.latitude.to_radians();
-	let b_latitude_rad = b.latitude.to_radians();
-	let d_latitude_rad = (a.latitude - b.latitude).to_radians();
-	let d_longitud_rad = (a.longitud - b.longitud).to_radians();
+impl Location {
+	/// Using the Haversine Formula from the course
+	fn distance(&self, other: &Location) -> f64 {
+		let a_latitude_rad = self.latitude.to_radians();
+		let b_latitude_rad = other.latitude.to_radians();
+		let d_latitude_rad = (self.latitude - other.latitude).to_radians();
+		let d_longitud_rad = (self.longitud - other.longitud).to_radians();
 
-	let inner_angle = (d_latitude_rad / 2.0).sin().powi(2) +
-		a_latitude_rad.cos() * b_latitude_rad.cos() *
-		(d_longitud_rad / 2.0).sin().powi(2);
+		let inner_angle = (d_latitude_rad / 2.0).sin().powi(2) +
+			a_latitude_rad.cos() * b_latitude_rad.cos() *
+			(d_longitud_rad / 2.0).sin().powi(2);
 
-	2.0 * EARTH_RADIUS * inner_angle.sqrt().asin()
+		2.0 * EARTH_RADIUS * inner_angle.sqrt().asin()
+	}
+}
+
+struct Waypoint<'l> {
+	code: &'l str,
+	location: &'l Location
+}
+
+impl Waypoint<'_> {
+	fn distance(&self, other: &Waypoint) -> f64 {
+		self.location.distance(other.location)
+	}
 }
 
 #[test]
 fn distance_test1() {
 	let loc1 = Waypoint {
-		latitude: 41.4075,
-		longitud: -81.851111,
+		code: "MEX",
+		location: &Location {
+			latitude: 41.4075,
+			longitud: -81.851111,
+		}
 	};
 	let loc2 = Waypoint {
-		latitude: 41.51030,
-		longitud: -83.88080,
+		code: "MEX",
+		location: &Location {
+			latitude: 41.51030,
+			longitud: -83.88080,
+		}
 	};
-	assert_eq!(format!("{:.2}", distance(&loc1, &loc2)), "169.52");
+	assert_eq!(format!("{:.2}", &loc1.distance(&loc2)), "169.52");
 }
+
 
 fn main() {
 	println!("Hello, world!");
