7940f2cd9495003fba41fd590278215dc231751c
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-12-01 01:59:29 -0600

    AdventOfCode/Rust: 2023-1 Part 2 initial attempt

--- AdventOfCode/rust/src/bin/2023-1.rs
+++ AdventOfCode/rust/src/bin/2023-1.rs
@@ -1,13 +1,34 @@
 use std::io;
 
-fn main() {
-	let lines = io::stdin().lines().map(|line| line.unwrap());
-	let ints = lines.map(|line| {
-		let digits: Vec<_> = line.chars().filter(|char| char.is_digit(10)).collect();
+fn recover(input: &String) -> i32 {
+		let digits: Vec<_> = input.chars().filter(|char| char.is_digit(10)).collect();
 		let result = format!("{}{}", digits.first().unwrap(), digits.last().unwrap());
 		result.parse::<i32>().unwrap()
-	});
-	let result: i32 = ints.sum();
+}
 
-	println!("{:#?}", result);
+fn replacement(input: &String) -> String {
+	input
+		.replace("one", "1")
+		.replace("two", "2")
+		.replace("three", "3")
+		.replace("four", "4")
+		.replace("five", "5")
+		.replace("six", "6")
+		.replace("seven", "7")
+		.replace("eight", "8")
+		.replace("nine", "9")
+}
+
+
+fn main() {
+	let lines: Vec<_> = io::stdin().lines().map(|line| line.unwrap()).collect();
+
+	let part1: i32 = lines.iter().map(recover).sum();
+	println!("Part 1: {}", part1);
+
+	let part2: i32 = lines.iter().map(|line| {
+		// TODO: oneight, eightwo, etc seems to throw a wrench at this approach
+		replacement(line)
+	}).map(|line| recover(&line)).sum();
+	println!("Part 2: {}", part2);
 }
